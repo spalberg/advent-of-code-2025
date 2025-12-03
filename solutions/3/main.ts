4824254e9597492ed83b4ec71e650ef64a1bbfe3
Sven Palberg <sven@palberg.de>
2025-12-03 07:52:31 +0100

    day 3

---
+++ solutions/3/main.ts
@@ -0,0 +1,53 @@
+import { defineSolution, runSolution } from "@/utils.ts";
+
+type BatteryBank = Array<number>;
+
+const solution = defineSolution((input) => {
+  const batteryBanks = input.map((line) => line.split("").map(Number));
+  return {
+    part1: () => solve(batteryBanks, 2),
+    part2: () => solve(batteryBanks, 12),
+  };
+});
+
+function solve(batteryBanks: Array<BatteryBank>, batteriesToTurnOn: number) {
+  let totalJoltage = 0;
+  for (const batteryBank of batteryBanks) {
+    let maxIndex = -1;
+    for (let i = 0; i < batteriesToTurnOn; i += 1) {
+      maxIndex = findMaxIndex(
+        batteryBank,
+        maxIndex + 1,
+        batteryBank.length - (batteriesToTurnOn - 1 - i),
+      );
+      const batteryJoltage = batteryBank[maxIndex] *
+        Math.pow(10, batteriesToTurnOn - 1 - i);
+      totalJoltage += batteryJoltage;
+    }
+  }
+  return totalJoltage;
+}
+
+function findMaxIndex(
+  batteryBank: BatteryBank,
+  start = 0,
+  end = batteryBank.length,
+) {
+  let maxIndex = -1;
+  let maxValue = -1;
+  for (let i = start; i < end; i += 1) {
+    const currentValue = batteryBank[i];
+    if (currentValue === 9) return i;
+    if (currentValue > maxValue) {
+      maxValue = currentValue;
+      maxIndex = i;
+    }
+  }
+  return maxIndex;
+}
+
+if (import.meta.main) {
+  runSolution(solution, 3);
+}
+
+export default solution;
---
+++ solutions/3/main_test.ts
@@ -0,0 +1,20 @@
+import { testDay } from "@/utils.ts";
+import solution from "./main.ts";
+
+await testDay(3, solution, async (test, loadInput) => {
+  await test(
+    "Example",
+    `
+    987654321111111
+    811111111111119
+    234234234234278
+    818181911112111
+    `,
+    { part1: 357, part2: 3121910778619 },
+  );
+  await test(
+    "Challange",
+    await loadInput(),
+    { part1: 17229, part2: 170520923035051 },
+  );
+});
